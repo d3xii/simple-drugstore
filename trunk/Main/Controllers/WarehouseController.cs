ae461f4a85d96ad6c5933239787e79a680442572
thang.tc.87@gmail.com <thang.tc.87@gmail.com@01eaf9cb-edb7-dc9d-f25f-eaea08bd5884>
2013-05-17 08:09:54 +0000

    reset source code

---
+++ trunk/Main/Controllers/WarehouseController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using SDM.Main.Models;
+
+namespace SDM.Main.Controllers
+{
+    public class WarehouseController : Controller
+    {
+        public ActionResult Items()
+        {
+            // create some items
+            List<TempItem> items = new List<TempItem>
+                {
+                    new TempItem {ID = 0, Name = "123"},
+                    new TempItem {ID = 0, Name = "abc"}
+                };
+
+            return View(items);
+        }
+    }
+}
---
+++ trunk/Main/Models/TempItem.cs
@@ -0,0 +1,8 @@
+﻿namespace SDM.Main.Models
+{
+    public class TempItem
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
