c8e497dc42b30b23c5963a0efe505483453340a4
thang.tc.87@gmail.com <thang.tc.87@gmail.com@01eaf9cb-edb7-dc9d-f25f-eaea08bd5884>
2013-05-17 09:22:21 +0000

    add Admin area add Core project

---
+++ trunk/Main/Areas/Admin/AdminAreaRegistration.cs
@@ -0,0 +1,30 @@
+﻿using System.Web.Mvc;
+
+namespace SDM.Main.Areas.Admin
+{
+    /// <summary>
+    /// Register area: "/admin".
+    /// </summary>
+    public class AdminAreaRegistration : AreaRegistration
+    {
+        /// <summary>
+        /// Gets the name of the area to register.
+        /// </summary>
+        /// <returns>
+        /// The name of the area to register.
+        /// </returns>
+        public override string AreaName
+        {
+            get { return "Admin"; }
+        }
+
+        /// <summary>
+        /// Registers an area in an ASP.NET MVC application using the specified area's context information.
+        /// </summary>
+        /// <param name="context">Encapsulates the information that is required in order to register the area.</param>
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute("Admin_default", "Admin/{controller}/{action}/{id}", new {controller = "AdminHome", action = "Index", id = UrlParameter.Optional});
+        }
+    }
+}
