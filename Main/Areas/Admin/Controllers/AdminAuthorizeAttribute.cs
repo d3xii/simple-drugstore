a2e62dda3a5a13ae9dc52dae64a06a6501caa4df
thang.tc.87 <thang.tc.87@gmail.com>
2013-05-17 17:18:22 +0000

    add [AdminAuthorizeAttribute] add <authentication> to Web.config

---
+++ Main/Areas/Admin/Controllers/AdminAuthorizeAttribute.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace SDM.Main.Areas.Admin.Controllers
+{
+    /// <summary>
+    /// Authorizes a request and redirects to approritated login page.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+    internal class AdminAuthorizeAttribute : AuthorizeAttribute
+    {
+        //**************************************************
+        //
+        // Public properties
+        //
+        //**************************************************
+
+        #region Public properties
+
+        /// <summary>
+        /// Gets name of the controller of the login page.
+        /// Default value is NULL which will use the same controller of declared class.
+        /// </summary>
+        public string Controller { get; private set; }
+
+        /// <summary>
+        /// Gets name of the action of the login page.
+        /// </summary>
+        public string Action { get; private set; }
+
+        #endregion
+
+        //**************************************************
+        //
+        // Constructors
+        //
+        //**************************************************
+
+        #region Constructors
+
+        /// <summary>
+        /// Marks given controller as [Authorize] and redirects Unauthorized Access to Login action of given controller.
+        /// </summary>
+        public AdminAuthorizeAttribute(string controller, string action)
+        {
+            Controller = controller;
+            Action = action;
+        }
+
+        /// <summary>
+        /// Marks given controller as [Authorize] and redirects Unauthorized Access to given action of the same controller.
+        /// </summary>
+        public AdminAuthorizeAttribute(string action) : this(null, action)
+        {
+        }
+
+        /// <summary>
+        /// Marks given controller as [Authorize] and redirects Unauthorized Access to Login action of the same controller.
+        /// </summary>
+        public AdminAuthorizeAttribute() : this("Login")
+        {
+        }
+
+        #endregion
+
+
+
+        #region Overrides of AuthorizeAttribute
+
+        /// <summary>
+        /// Called when a process requests authorization.
+        /// </summary>
+        /// <param name="filterContext">The filter context, which encapsulates information for using <see cref="T:System.Web.Mvc.AuthorizeAttribute"/>.</param><exception cref="T:System.ArgumentNullException">The <paramref name="filterContext"/> parameter is null.</exception>
+        public override void OnAuthorization(AuthorizationContext filterContext)
+        {
+            // call base method
+            base.OnAuthorization(filterContext);
+
+            // OK?
+            if (!(filterContext.Result is HttpUnauthorizedResult))
+            {
+                // ok
+                return;
+            }
+
+            // prepare parameter
+            var routeValues = new RouteValueDictionary
+                {
+                    {"action", "Login"},
+                    {"ReturnUrl", filterContext.HttpContext.Request.RawUrl}
+                };
+
+            // if the controller is defined, add it to route values
+            if (!string.IsNullOrEmpty(this.Controller))
+            {
+                routeValues.Add("controller", this.Controller);
+            }
+
+            // redirect
+            filterContext.Result = new RedirectToRouteResult(routeValues);
+        }
+
+        #endregion
+    }
+}
--- Main/Areas/Admin/Controllers/AdminHomeController.cs
+++ Main/Areas/Admin/Controllers/AdminHomeController.cs
@@ -5,12 +5,18 @@ namespace SDM.Main.Areas.Admin.Controllers
     /// <summary>
     /// Represents the Home.
     /// </summary>
+    [AdminAuthorize]
     public class AdminHomeController : Controller
     {
         public ActionResult Index()
         {
             return View();
         }
-
+        
+        [AllowAnonymous]
+        public string Login(string returningUrl)
+        {
+            return "From " + returningUrl;
+        }
     }
 }
