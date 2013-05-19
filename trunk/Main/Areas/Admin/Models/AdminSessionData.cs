951416066e85f529900f17f36c8a26314fa5140d
thang.tc.87@gmail.com <thang.tc.87@gmail.com@01eaf9cb-edb7-dc9d-f25f-eaea08bd5884>
2013-05-19 05:01:55 +0000

    finished: save user to cookie after logging in

---
+++ trunk/Main/Areas/Admin/Models/AdminSessionData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SDM.Main.Areas.Admin.Models
+{
+    /// <summary>
+    /// Contains session data of Admin area.
+    /// </summary>
+    public class AdminSessionData
+    {
+    }
+}
