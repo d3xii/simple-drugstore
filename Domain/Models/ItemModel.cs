b3ac62781f77014decb2b57cf5bcee0cc83e8f3f
thang.tc.87 <thang.tc.87@gmail.com>
2013-06-17 09:47:53 +0000

    add Items Management screen

--- Domain/Models/ItemModel.cs
+++ Domain/Models/ItemModel.cs
@@ -1,5 +1,9 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using SDM.Core.Localization;
 using SDM.Domain.Models.Base;
+using SDM.Localization.Core;
 
 namespace SDM.Domain.Models
 {
@@ -7,11 +11,80 @@ namespace SDM.Domain.Models
     /// Contains information of an item in the database.
     /// </summary>
     [Table("Item")]
-    public class ItemModel : ModelBase
+    public class ItemModel : ModelBase, ILocalizable<ItemModel.Texts>
     {
+        //**************************************************
+        //
+        // Public methods
+        //
+        //**************************************************
+
+        #region Public methods
+
         /// <summary>
         /// Gets or sets full name of the item.
         /// </summary>
+        [Required]
         public string Name { get; set; }
+
+        #endregion
+
+
+        //**************************************************
+        //
+        // Public methods
+        //
+        //**************************************************
+
+        #region Public methods        
+
+        /// <summary>
+        /// Validates and creates new item.
+        /// </summary>
+        public string Create(IDbSet<ItemModel> items)
+        {
+            // not duplicated name
+            if (items.IsExisted(this.Name))
+            {
+                // duplicated
+                return this.Localize(t => t.DuplicatedName);
+            }
+
+            // just add, no validation
+            items.Add(this);
+            return null;
+        }
+
+        /// <summary>
+        /// Updates this model with information contained in the dummy model.
+        /// </summary>
+        public string Update(ItemModel model)
+        {
+            // update other information
+            this.Name = model.Name;
+
+            // ok
+            return null;
+        }
+
+        #endregion
+
+
+        //**************************************************
+        //
+        // Nested classes
+        //
+        //**************************************************
+
+        #region Nested classes
+
+        public class Texts : CustomLocalizationScopeBase
+        {
+            public string Name = "Name";
+
+            public string DuplicatedName = "The name has been used. Please choose another one.";
+        }
+
+        #endregion        
     }
 }
---
+++ Domain/Models/ItemRepository.cs
@@ -0,0 +1,40 @@
+﻿using System.Data.Entity;
+using System.Linq;
+
+namespace SDM.Domain.Models
+{
+    /// <summary>
+    /// Provides database access of <see cref="ItemModel"/>.
+    /// </summary>
+    public static class ItemRepository
+    {
+        //**************************************************
+        //
+        // Public methods
+        //
+        //**************************************************
+
+        #region Public methods
+
+        /// <summary>
+        /// Checks whether given name existed in the database.
+        /// The user name is case-insensitive.
+        /// This method has been optimized.
+        /// </summary>
+        public static bool IsExisted(this IDbSet<ItemModel> models, string name)
+        {
+            return models.Any(t => t.Name.ToLower() == name.ToLower());
+        }
+
+        /// <summary>
+        /// Gets model by given user name.
+        /// The user name is case-insensitive.
+        /// </summary>
+        public static ItemModel GetByName(this IDbSet<ItemModel> models, string name)
+        {
+            return models.SingleOrDefault(t => t.Name.ToLower() == name.ToLower());
+        }
+
+        #endregion
+    }
+}
